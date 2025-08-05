58890e509e3592316c5f0eae37be191f59efe5df
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-08-05 11:09:47 -0400

    first commit - spring initialize
    Add files/archives generated by spring initialize

---
+++ src/main/java/br/com/norteautopecas/painel_administrativo_backend/PainelAdministrativoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.norteautopecas.painel_administrativo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainelAdministrativoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainelAdministrativoBackendApplication.class, args);
+	}
+
+}
