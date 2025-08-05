3a5c89686ce552d68b72ddeca970a18efc17e10c
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-08-05 15:42:17 -0400

    feat: adicionando configurações de cors

---
+++ src/main/java/br/com/norteautopecas/painel_administrativo_backend/config/CorsConfiguration.java
@@ -0,0 +1,14 @@
+package br.com.norteautopecas.painel_administrativo_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
