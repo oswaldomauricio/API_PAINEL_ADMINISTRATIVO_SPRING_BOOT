3eba7041453a222733e96c3e44d39381202f2d1b
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-08-06 00:04:53 -0400

    feat: creating the Store Entity e Store Repository

---
+++ src/main/java/br/com/norteautopecas/painel_administrativo_backend/infra/repository/user/StoreRepository.java
@@ -0,0 +1,7 @@
+package br.com.norteautopecas.painel_administrativo_backend.infra.repository.user;
+
+import br.com.norteautopecas.painel_administrativo_backend.infra.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
