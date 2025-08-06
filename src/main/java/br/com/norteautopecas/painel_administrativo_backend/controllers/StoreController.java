9b3c8ee4bf21af00c7c4b70c29ee7a6f5e849d5f
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-08-06 12:21:20 -0400

    feat:  adding the route to list user by id

--- src/main/java/br/com/norteautopecas/painel_administrativo_backend/controllers/StoreController.java
+++ src/main/java/br/com/norteautopecas/painel_administrativo_backend/controllers/StoreController.java
@@ -1,18 +1,20 @@
 package br.com.norteautopecas.painel_administrativo_backend.controllers;
 
 import br.com.norteautopecas.painel_administrativo_backend.bussines.StoreService;
+import br.com.norteautopecas.painel_administrativo_backend.infra.dto.ListStoreByUserDTO;
 import br.com.norteautopecas.painel_administrativo_backend.infra.dto.RegisterStoreDTO;
 import br.com.norteautopecas.painel_administrativo_backend.infra.dto.StoreRegistrationDetailsDTO;
+import br.com.norteautopecas.painel_administrativo_backend.infra.repository.user.StoreRepository;
+import br.com.norteautopecas.painel_administrativo_backend.infra.validations.ValidateException;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/lojas")
@@ -20,6 +22,9 @@ public class StoreController {
 
     @Autowired
     private StoreService storeService;
+
+    @Autowired
+    private StoreRepository storeRepository;
 
     @PostMapping
     @Transactional
@@ -29,5 +34,11 @@ public class StoreController {
         var store = storeService.registerStore(dados);
 
         return ResponseEntity.created(null).body(store);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<ListStoreByUserDTO>> listStoresByUser(@PathVariable Long id) {
+        var stores = storeService.listStoresByUser(id);
+        return ResponseEntity.ok().body(stores);
     }
 }
