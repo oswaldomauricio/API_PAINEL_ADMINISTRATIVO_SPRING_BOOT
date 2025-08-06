829abd49e67088c9ed88d8f1e1290dd3bf9f67cb
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-08-06 11:25:10 -0400

    refactor: updating StoreRepository to existes users with store register

--- src/main/java/br/com/norteautopecas/painel_administrativo_backend/infra/repository/user/StoreRepository.java
+++ src/main/java/br/com/norteautopecas/painel_administrativo_backend/infra/repository/user/StoreRepository.java
@@ -4,4 +4,5 @@ import br.com.norteautopecas.painel_administrativo_backend.infra.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    boolean existsByIdAndLoja(Long id, Integer loja);
 }
