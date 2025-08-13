fd2d0d408bf03fb71ff982ed2538e1ce0af052d2
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-08-13 12:15:14 -0400

    style: creating the getters and setters in storeInformation

--- src/main/java/br/com/norteautopecas/painel_administrativo_backend/infra/entity/StoreInformation.java
+++ src/main/java/br/com/norteautopecas/painel_administrativo_backend/infra/entity/StoreInformation.java
@@ -62,4 +62,132 @@ public class StoreInformation {
 
     @Column(name = "horario")
     private String horario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getLoja() {
+        return loja;
+    }
+
+    public void setLoja(Integer loja) {
+        this.loja = loja;
+    }
+
+    public String getNomeLoja() {
+        return nomeLoja;
+    }
+
+    public void setNomeLoja(String nomeLoja) {
+        this.nomeLoja = nomeLoja;
+    }
+
+    public String getEnderecoLoja() {
+        return enderecoLoja;
+    }
+
+    public void setEnderecoLoja(String enderecoLoja) {
+        this.enderecoLoja = enderecoLoja;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSegmentacao() {
+        return segmentacao;
+    }
+
+    public void setSegmentacao(String segmentacao) {
+        this.segmentacao = segmentacao;
+    }
+
+    public String getSegmentacao2() {
+        return segmentacao2;
+    }
+
+    public void setSegmentacao2(String segmentacao2) {
+        this.segmentacao2 = segmentacao2;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
 }
