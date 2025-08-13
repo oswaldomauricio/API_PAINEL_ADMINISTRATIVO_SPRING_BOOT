4f29c3f828a0904d9a1e1798692ee89d8b624ad3
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-08-13 00:03:07 -0400

    Add ProdutoCreateDTO and update Produto entity
    Introduces ProdutoCreateDTO for product creation requests and adds constructors, getters, and setters to the Produto entity to support new fields and improve data handling.

--- src/main/java/br/com/norteautopecas/painel_administrativo_backend/infra/entity/Produto.java
+++ src/main/java/br/com/norteautopecas/painel_administrativo_backend/infra/entity/Produto.java
@@ -14,9 +14,12 @@ public class Produto {
 
     @Column(name = "codigo_produto", nullable = false)
     private String codigoProduto;
+
     @Column(name = "quantidade", nullable = false)
     private Integer quantidade;
+
     private Tipo tipo;
+
     @Column(name = "valor_unitario")
     private BigDecimal valorUnitario;
 
@@ -25,4 +28,75 @@ public class Produto {
 
     @ManyToOne
     private TicketDivergencia ticketDivergencia;
+
+    public Produto(
+            String codigoProduto,
+            Integer quantidade,
+            Tipo tipo,
+            BigDecimal valorUnitario,
+            TicketGarantia ticketGarantia
+    ) {
+        this.id = null;
+        this.codigoProduto = codigoProduto;
+        this.quantidade = quantidade;
+        this.tipo = tipo;
+        this.valorUnitario = valorUnitario;
+        this.ticketGarantia = ticketGarantia;
+    }
+
+    public TicketGarantia getTicketGarantia() {
+        return ticketGarantia;
+    }
+
+    public void setTicketGarantia(TicketGarantia ticketGarantia) {
+        this.ticketGarantia = ticketGarantia;
+    }
+
+    public TicketDivergencia getTicketDivergencia() {
+        return ticketDivergencia;
+    }
+
+    public void setTicketDivergencia(TicketDivergencia ticketDivergencia) {
+        this.ticketDivergencia = ticketDivergencia;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public void setCodigoProduto(String codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public BigDecimal getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(BigDecimal valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
 }
