88dd8a1893b1a7b3749f9acbf47ff2c7c7c01a50
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-08-20 00:46:59 -0400

    fix: Updated the CORS configuration to allow requests from any origin by changing allowedOrigins to '*'. Also implemented WebMvcConfigurer to properly override addCorsMappings.
    Updated the CORS configuration to allow requests from any origin by changing allowedOrigins to '*'. Also implemented WebMvcConfigurer to properly override addCorsMappings.

--- src/main/java/br/com/norteautopecas/painel_administrativo_backend/config/CorsConfiguration.java
+++ src/main/java/br/com/norteautopecas/painel_administrativo_backend/config/CorsConfiguration.java
@@ -5,10 +5,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
