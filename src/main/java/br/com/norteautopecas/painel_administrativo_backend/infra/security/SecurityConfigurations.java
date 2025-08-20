4fe68a5773a03c39b65ff18292e7dac95ddc6be7
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-08-20 00:47:29 -0400

    refactor: Changed the permitted login endpoint from '/login' to '/v1/login' in the security filter chain to reflect the updated API route.
    Changed the permitted login endpoint from '/login' to '/v1/login' in the security filter chain to reflect the updated API route.

--- src/main/java/br/com/norteautopecas/painel_administrativo_backend/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/norteautopecas/painel_administrativo_backend/infra/security/SecurityConfigurations.java
@@ -23,7 +23,7 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable()).sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(req -> {
-            req.requestMatchers("/login").permitAll();
+            req.requestMatchers("/v1/login").permitAll();
             req.requestMatchers("/v3/api-docs/**", "swagger-ui.html",
                     "swagger-ui/**").permitAll();
             req.anyRequest().authenticated();
